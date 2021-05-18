d6fa1ef4d3d9f762acfe98dc57c8240b177ac439
Richard Marques <rmarqu65@ford.com>
2021-05-18 18:47:32 -0300

    build(docker): docker compose with postgres

--- src/main/java/com/seberino/chat/entity/User.java
+++ src/main/java/com/seberino/chat/entity/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "user_info")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
